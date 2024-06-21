a553d90748bfa904364baab40583da1b6a8e0847
Harris <16213031+Labhatorian@users.noreply.github.com>
2024-06-21 15:55:50 +0200

    show login screen

--- aftekenen/app/_layout.tsx
+++ aftekenen/app/_layout.tsx
@@ -2,36 +2,67 @@ import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
+import * as SecureStore from 'expo-secure-store';
+import { View, Text } from 'react-native';
+import { Camera, CameraView } from 'expo-camera'; // Import expo-camera
 
-// Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
+  const [loaded, setLoaded] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const [fontsLoaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
 
   useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
+    const checkAuthentication = async () => {
+      try {
+        const result = await SecureStore.getItemAsync('authentication');
+        if (result !== null) {
+          setIsLoggedIn(true);
+        }
+      } catch (error) {
+        console.error('Error checking authentication:', error);
+      } finally {
+        setLoaded(true);
+        SplashScreen.hideAsync();
+      }
+    };
 
-  if (!loaded) {
-    return null;
+    checkAuthentication();
+  }, []);
+
+  if (!fontsLoaded || !loaded) {
+    return null; // or render a loading indicator
   }
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
+      {isLoggedIn ? (
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="+not-found" />
+        </Stack>
+      ) : (
+        <View style={{ flex: 1 }}>
+          <Text style={{ alignSelf: 'center', marginTop: 50 }}>U moet een QR-code scannen om de app te kunnen gebruiken. De QR-code is naar u verzonden.</Text>
+          <View style={{ flex: 1 }}>
+            <CameraView
+              style={{ flex: 1 }}
+              // onBarcodeScanned={scanned ? undefined : handleBarCodeScanned}
+              barcodeScannerSettings={{
+                barcodeTypes: ["qr"],
+              }} />
+          </View>
+        </View>
+      )}
     </ThemeProvider>
   );
 }
